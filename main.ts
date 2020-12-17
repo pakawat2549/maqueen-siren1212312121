0c781afe47e3ddffe47f132610deab1d8155bdad
pakawat2549 <75378790+pakawat2549@users.noreply.github.com>
2020-12-17 22:43:03 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -47,14 +47,19 @@ basic.forever(function () {
     }
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
         while (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
             basic.pause(500)
+            maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
             basic.pause(500)
+            maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-            basic.pause(1000)
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
             basic.pause(500)
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+            basic.pause(1000)
+            maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
         }
     }
