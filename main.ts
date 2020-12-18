11bacf8d1d7b8815062d0a28c12f64829dcf6cf1
pakawat2549 <75378790+pakawat2549@users.noreply.github.com>
2020-12-18 21:28:46 +0700

    หลบของ

--- main.ts
+++ main.ts
@@ -6,61 +6,31 @@ basic.forever(function () {
     basic.pause(100)
 })
 basic.forever(function () {
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . . . . .
-        . # # # .
-        . . . . .
-        `)
-    basic.showLeds(`
-        # # # # .
-        # . . . .
-        # . # # .
-        # . . # .
-        # # # # .
-        `)
-    basic.showLeds(`
-        # # # # .
-        # . . # .
-        # . . # .
-        # . . # .
-        # # # # .
-        `)
-    basic.showLeds(`
-        # # . . .
-        # . # . .
-        # . # . .
-        # . # . .
-        # # . . .
-        `)
+    basic.showNumber(maqueen.Ultrasonic(PingUnit.Centimeters))
 })
 basic.forever(function () {
-    if (pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 0) {
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-    } else if (pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 0) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
-    } else if (pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 1) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-    }
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 15) {
         while (maqueen.Ultrasonic(PingUnit.Centimeters) < 20) {
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
-            basic.pause(500)
+            basic.pause(600)
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-            basic.pause(500)
+            basic.pause(600)
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
-            basic.pause(500)
+            basic.pause(700)
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
-            basic.pause(1000)
+            basic.pause(600)
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+            basic.pause(700)
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+            basic.pause(600)
+            maqueen.motorStop(maqueen.Motors.All)
+            basic.pause(1000000000000)
         }
     }
 })
